36d0ba44430bf979b5e034cf3290e80264cad541
renato90 <renato010990@gmail.com>
2023-08-06 22:23:15 -0700

    script6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,18 +1,15 @@
-# include <stdio.h>
-
+#include <stdio.h>
 /**
- *  *main - beginning of program
- *   *
- *    *Description: prints a string
- *     *
- *      *Return: 0
- *      */
+* main - printthe sizeof various types onthe computer itis compiled and runon
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-		printf("Size of a char: %zu byte(s)\n", sizeof(char));
-			printf("Size of an int: %zu byte(s)\n", sizeof(int));
-				printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
-						printf("Size of a float: %zu byte(s)\n", sizeof(float));
-							return (0);
-
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
