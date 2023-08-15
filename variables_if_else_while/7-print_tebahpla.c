b73004d841f6144c4d0c3450ca3ebc7cc8064a00
renato90 <renato010990@gmail.com>
2023-08-14 17:43:26 -0700

    script7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  * main - Prints all the alphabet from z to a
+ *   * Return: Always (Success)
+ *    */
+
+int main(void)
+{
+		char l;
+
+			for (l = 'z'; l >= 'a'; l--)
+					putchar(l);
+				putchar('\n');
+
+					return (0);
+}
