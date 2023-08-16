e38124e9d73ab44ff0529cf11cad846bf828b7da
renato90 <renato010990@gmail.com>
2023-08-15 19:27:45 -0700

    script1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  * print_alphabet - Make the alphabet
+ *   * Description: "this program prints the alphabet"
+ *    * return: void
+ *     */
+
+void print_alphabet(void)
+{
+		char c;
+
+			for (c = 'a'; c <= 'z'; c++)
+					{
+							_putchar(c);
+								}
+				_putchar('\n');
+}
